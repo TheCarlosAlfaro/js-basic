f03f8850e24215be6efb58b2b0e3704c1fa5cbb1
carlosalfarodev <rgkalin@gmail.com>
2020-12-29 06:02:14 -0800

    functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// function declaration
+function myFunction() {
+	console.log("hi");
+	return 3;
+}
+
+// function expression
+var myFunction = function (a, b) {
+	console.log("hi again");
+	return a + b;
+};
+
+myFunction(3, 4);

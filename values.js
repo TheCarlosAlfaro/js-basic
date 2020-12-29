b5bc5fd93976370308a90792c9817f3258a7becf
carlosalfarodev <rgkalin@gmail.com>
2020-12-29 05:26:33 -0800

    variables

--- values.js
+++ values.js
@@ -1,9 +1,16 @@
-40;
+// Primitive values
+// number
+typeof 40;
+// string
+typeof "Carlos Alfaro";
 
-("Carlos Alfaro");
+// boolean
+typeof true;
+typeof false;
 
-true;
-false;
+typeof null;
+typeof undefined;
 
-null;
-undefined;
+// non primitive or object values
+typeof [1, 2, 3, 4];
+typeof { firstName: "Carlos", lastName: "Alfaro", age: 32, eyeColor: "Black" };
---
+++ variable.js
@@ -0,0 +1,13 @@
+var name = "Bri";
+
+// Declare a variable
+var age;
+// initialize a variable
+age = 32;
+
+var elements = ["computer", "phone"];
+
+var person = {
+	name: "Carlos",
+	age: 32,
+};
